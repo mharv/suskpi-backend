6e6df5698ebb0bff604a1603230b3e268813de12
Mitchell Harvey <mitch.k.harvey@gmail.com>
2021-09-19 09:00:58 +1000

    testing environment variables

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
 	"sus-kpi-backend/controllers"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	dotenvtest := goDotEnvVariable("TEST")
+	fmt.Printf(dotenvtest + "\n")
+
 	router := gin.Default()
 	router.GET("/albums", controllers.GetAlbums)
 	router.GET("/albums/:id", controllers.GetAlbumByID)
@@ -15,4 +22,16 @@ func main() {
 	router.Run("localhost:8080")
 }
 
-// albums slice to seed record album data.
+// use godot package to load/read the .env file and
+// return the value of the key
+func goDotEnvVariable(key string) string {
+
+	// load .env file
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
