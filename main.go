1a8998d80c00e0eb041c7f528da589e798b0737b
Mitchell Harvey <mitch.k.harvey@gmail.com>
2021-09-19 13:53:36 +1000

    remove tutorial stuff

--- main.go
+++ main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"sus-kpi-backend/controllers"
-	"sus-kpi-backend/db"
 
 	_ "github.com/denisenkom/go-mssqldb"
 
@@ -12,19 +9,15 @@ import (
 )
 
 func main() {
-	db.ConnectToDb()
-
-	count, err := db.ReadBauTable()
-	if err != nil {
-		log.Fatal("Error reading bau benchmarks: ", err.Error())
-	}
-	fmt.Printf("Read %d row(s) successfully.\n", count)
 
 	// Routes
 	router := gin.Default()
-	router.GET("/albums", controllers.GetAlbums)
-	router.GET("/albums/:id", controllers.GetAlbumByID)
-	router.POST("/albums", controllers.PostAlbums)
+	router.GET("/bauBenchmarks", controllers.GetBauBenchmarks)
+	router.GET("/projects", controllers.GetProjects)
+	// examples
+
+	// router.GET("/albums/:id", controllers.GetAlbumByID)
+	// router.POST("/albums", controllers.PostAlbums)
 
 	router.Run("localhost:8080")
 }
