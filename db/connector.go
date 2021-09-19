e3568e1a7f4c5b763639062eaaff749be75c599e
Mitchell Harvey <mitch.k.harvey@gmail.com>
2021-09-19 13:52:42 +1000

    convert sql output to json for route response

--- db/connector.go
+++ db/connector.go
@@ -3,6 +3,7 @@ package db
 import (
 	"context"
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -11,6 +12,8 @@ import (
 
 	"github.com/joho/godotenv"
 )
+
+// https://docs.microsoft.com/en-us/azure/azure-sql/database/connect-query-go
 
 var db *sql.DB
 
@@ -40,42 +43,65 @@ func ConnectToDb() {
 	fmt.Printf("Connected!\n")
 }
 
-func ReadBauTable() (int, error) {
+// examples
+
+// // postAlbums adds an album from JSON received in the request body.
+// func PostAlbums(c *gin.Context) {
+// 	var newAlbum models.Album
+
+// 	// Call BindJSON to bind the received JSON to
+// 	// newAlbum.
+// 	if err := c.BindJSON(&newAlbum); err != nil {
+// 		return
+// 	}
+
+// 	// Add the new album to the slice.
+// 	albums = append(albums, newAlbum)
+// 	c.IndentedJSON(http.StatusCreated, newAlbum)
+// }
+
+// // getAlbumByID locates the album whose ID value matches the id
+// // parameter sent by the client, then returns that album as a response.
+// func GetAlbumByID(c *gin.Context) {
+// 	id := c.Param("id")
+
+// 	// Loop over the list of albums, looking for
+// 	// an album whose ID value matches the parameter.
+// 	for _, a := range albums {
+// 		if a.ID == id {
+// 			c.IndentedJSON(http.StatusOK, a)
+// 			return
+// 		}
+// 	}
+// 	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+// }
+
+func GetData(table string) (string, error) {
 	ctx := context.Background()
 
 	// Check if database is alive.
 	err := db.PingContext(ctx)
 	if err != nil {
-		return -1, err
+		return "-1", err
 	}
 
-	tsql := fmt.Sprintf("SELECT BauRef FROM dbo.BauTable;")
+	tsql := fmt.Sprintf("SELECT * FROM %s;", table)
 
 	// Execute query
 	rows, err := db.QueryContext(ctx, tsql)
 	if err != nil {
-		return -1, err
+		return "-1", err
 	}
-
 	defer rows.Close()
 
-	var count int
-
-	// Iterate through the result set.
-	for rows.Next() {
-		var BauRef string
-
-		// Get values from row.
-		err := rows.Scan(&BauRef)
-		if err != nil {
-			return -1, err
-		}
-
-		fmt.Printf("BauRef: %s\n", BauRef)
-		count++
+	columns, err := rows.Columns()
+	if err != nil {
+		return "-1", err
 	}
 
-	return count, nil
+	result, err := convertToJson(rows, columns)
+
+	return result, err
 }
 
 // use godot package to load/read the .env file and
@@ -90,4 +116,36 @@ func goDotEnvVariable(key string) string {
 	}
 
 	return os.Getenv(key)
+}
+
+// https://www.sohamkamani.com/golang/json/
+func convertToJson(rows *sql.Rows, columns []string) (string, error) {
+	count := len(columns)
+	tableData := make([]map[string]interface{}, 0)
+	values := make([]interface{}, count)
+	valuePtrs := make([]interface{}, count)
+	for rows.Next() {
+		for i := 0; i < count; i++ {
+			valuePtrs[i] = &values[i]
+		}
+		rows.Scan(valuePtrs...)
+		entry := make(map[string]interface{})
+		for i, col := range columns {
+			var v interface{}
+			val := values[i]
+			b, ok := val.([]byte)
+			if ok {
+				v = string(b)
+			} else {
+				v = val
+			}
+			entry[col] = v
+		}
+		tableData = append(tableData, entry)
+	}
+	jsonData, err := json.Marshal(tableData)
+	if err != nil {
+		return "", err
+	}
+	return string(jsonData), nil
 }
